19bb767213df05f2a9e21402657a6c42c3521aa7
Tuguldur T <146443087+tuuguu556@users.noreply.github.com>
2026-09-01 21:07:14 +0800

    Bayer-4x4 algorithm, some UI changes, removed old ui text

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,11 @@ import { useEffect, useRef, useState } from "react";
 export default function Home() {
   const [imageSrc, setImageSrc] = useState(null);
   const [focused, setFocused] = useState(0); // 0 is view, 1 is controls, 2 is pallete
+  const algorithms = ["Bayer 4x4", "Floyd-Steinberg"];
+  const [isOpen, setIsOpen] = useState(false);
+  const [algorithmsSelected, setAlgorithmsSelected] = useState(null);
+  const dropdownRef = useRef(null);
+
   const canvasRef = useRef(null);
   const loadedImgRef = useRef(null);
 
@@ -36,6 +41,108 @@ export default function Home() {
     return () => window.removeEventListener("keydown", handleKeydown);
   }, []);
 
+  useEffect(() => {
+    const handleDropdownExit = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleDropdownExit);
+    return () => document.removeEventListener("mousedown", handleDropdownExit);
+  }, []);
+
+  useEffect(() => {
+    if (loadedImgRef.current) {
+      drawCanvas(loadedImgRef.current);
+    }
+  }, [algorithmsSelected]);
+
+  const handleSelect = (option) => {
+    setAlgorithmsSelected(option);
+    setIsOpen(false);
+  };
+
+  const findClosestColor = (r, g, b, pallete) => {
+    //finds closest color using super special 3d distance formula that i always forget
+    let minDistance = Infinity;
+    let closestColor = pallete[0];
+
+    for (let i = 0; i < pallete.length; i++) {
+      const [pr, pg, pb] = pallete[i];
+      const distance = (r - pr) ** 2 + (g - pg) ** 2 + (b - pb) ** 2;
+
+      if (distance < minDistance) {
+        minDistance = distance;
+        closestColor = pallete[i];
+      }
+    }
+    return closestColor;
+  };
+
+  const floydsteinberg = (data, width, height, pallete) => {
+    const calculateError = (nx, ny, errR, errG, errB, factor) => {
+      if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+        let nIdx = (ny * width + nx) * 4;
+        data[nIdx] += errR * factor;
+        data[nIdx + 1] += errG * factor;
+        data[nIdx + 2] += errB * factor;
+      }
+    };
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        let index = (y * width + x) * 4; // this is so i dont have to turn the 1d array from data to a 2d array
+
+        let oldR = data[index];
+        let oldG = data[index + 1];
+        let oldB = data[index + 2];
+
+        const [newR, newG, newB] = findClosestColor(oldR, oldG, oldB, pallete);
+
+        data[index] = newR;
+        data[index + 1] = newG;
+        data[index + 2] = newB;
+
+        let errR = oldR - newR;
+        let errG = oldG - newG;
+        let errB = oldB - newB;
+
+        calculateError(x + 1, y, errR, errG, errB, 7 / 16); // floyd-steinberg weights eg. 7/16
+        calculateError(x - 1, y + 1, errR, errG, errB, 3 / 16);
+        calculateError(x, y + 1, errR, errG, errB, 5 / 16);
+        calculateError(x + 1, y + 1, errR, errG, errB, 1 / 16);
+      }
+    }
+  };
+
+  const bayer = (data, width, height, pallete) => {
+    const bayer_4x4 = [
+      [0, 8, 2, 10],
+      [12, 4, 14, 6],
+      [3, 11, 1, 9],
+      [15, 7, 13, 5],
+    ];
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        let index = (y * width + x) * 4;
+
+        const matrix = bayer_4x4[y % 4][x % 4];
+
+        const bias = (matrix / 16 - 0.5) * 64;
+
+        let r = Math.min(255, Math.max(0, data[index] + bias));
+        let g = Math.min(255, Math.max(0, data[index + 1] + bias));
+        let b = Math.min(255, Math.max(0, data[index + 2] + bias));
+
+        const [newR, newG, newB] = findClosestColor(r, g, b, pallete);
+
+        data[index] = newR;
+        data[index + 1] = newG;
+        data[index + 2] = newB;
+      }
+    }
+  };
+
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     if (!file) return;
@@ -62,26 +169,10 @@ export default function Home() {
     link.click();
   };
 
-  const findClosestColor = (r, g, b, pallete) => {
-    //finds closest color using super special 3d distance formula that i always forget
-    let minDistance = Infinity;
-    let closestColor = pallete[0];
-
-    for (let i = 0; i < pallete.length; i++) {
-      const [pr, pg, pb] = pallete[i];
-      const distance = (r - pr) ** 2 + (g - pg) ** 2 + (b - pb) ** 2;
-
-      if (distance < minDistance) {
-        minDistance = distance;
-        closestColor = pallete[i];
-      }
-    }
-    return closestColor;
-  };
-
   const drawCanvas = (img) => {
     const canvas = canvasRef.current;
     if (!canvas) return;
+
     const ctx = canvas.getContext("2d");
     let width = img.width;
     let height = img.height;
@@ -91,62 +182,14 @@ export default function Home() {
     ctx.drawImage(img, 0, 0);
     let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const data = imageData.data;
-    // ok some confusing math is coming so bear with me
 
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        let index = (y * width + x) * 4; // this is so i dont have to turn the 1d array from data to a 2d array
-
-        let oldR = data[index];
-        let oldG = data[index + 1];
-        let oldB = data[index + 2];
-
-        const [newR, newG, newB] = findClosestColor(oldR, oldG, oldB, pallete);
-
-        data[index] = newR;
-        data[index + 1] = newG;
-        data[index + 2] = newB;
-
-        let errR = oldR - newR;
-        let errG = oldG - newG;
-        let errB = oldB - newB;
-
-        const calculateError = (nx, ny, errR, errG, errB, factor) => {
-          if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
-            let nIdx = (ny * width + nx) * 4;
-            data[nIdx] += errR * factor;
-            data[nIdx + 1] += errG * factor;
-            data[nIdx + 2] += errB * factor;
-          }
-        };
-
-        calculateError(x + 1, y, errR, errG, errB, 7 / 16); // floyd-steinberg weights eg. 7/16
-        calculateError(x - 1, y + 1, errR, errG, errB, 3 / 16);
-        calculateError(x, y + 1, errR, errG, errB, 5 / 16);
-        calculateError(x + 1, y + 1, errR, errG, errB, 1 / 16);
-      }
+    if (algorithmsSelected == "Bayer 4x4") {
+      bayer(data, width, height, pallete);
+    } else if (algorithmsSelected == "Floyd-Steinberg") {
+      floydsteinberg(data, width, height, pallete);
     }
     ctx.putImageData(imageData, 0, 0);
   };
-  //   return (
-  //     <div className="min-h-screen bg-gray-900 text-white p-8 flex flex-col items-center">
-  //       <h1 className="text-4xl font-bold mb-2">Stipplr</h1>
-  //       <p className="text-gray-400 mb-8">Dithering & Image processing</p>
-  //       <div>
-  //         <input
-  //           type="file"
-  //           accept="image/*"
-  //           onChange={handleImageUpload}
-  //           className="file:mr-4 file:py-2 file:px-4 file:rounded-full file:bg-violet-50 file:text-sm file:text-black cursor-pointer"
-  //         ></input>
-  //         <div>
-  //           <canvas ref={canvasRef} className="max-w-full h-auto" />
-  //         </div>
-  //         <button onClick={handleExport}>Export</button>
-  //       </div>
-  //     </div>
-  //   );
-  // }
 
   return (
     <div className="h-screen w-screen bg-[#0d0d0d] text-[#d0d0d0] font-mono p-3 flex flex-col gap-2 select-none overflow-hidden">
@@ -174,7 +217,7 @@ export default function Home() {
             className={`px-2 py-1 text-xs flex justify-between font-bold ${focused === 0 ? "bg-[#a3ff00] text-black" : "bg-[#202020] text-gray-500"}`}
           >
             <p>View</p>
-            <p>floyd-steinberg</p>
+            <p>{algorithmsSelected}</p>
           </div>
           <div className="flex-1 p-4 flex items-center justify-center bg-black overflow-auto relative">
             {!imageSrc && (
@@ -217,6 +260,28 @@ export default function Home() {
                   Export PNG
                 </button>
               )}
+              <div ref={dropdownRef} className="relative">
+                <button
+                  className="border px-2 py-1 w-fit"
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  {algorithmsSelected || "Select an option"}
+                  <span className={`arrow ${isOpen ? "open" : ""}`}> &lt;</span>
+                </button>
+                {isOpen && (
+                  <ul>
+                    {algorithms.map((option, index) => (
+                      <li
+                        className={`hover:text-[#a3ff00] ${algorithmsSelected == option ? "text-[#a3ff00]" : ""}`}
+                        key={index}
+                        onClick={() => handleSelect(option)}
+                      >
+                        &gt; {option}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
             </div>
           </div>
           {/* 2nd, technically 3rd window */}
